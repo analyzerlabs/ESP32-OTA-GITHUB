d7c2520966c5018835ed905f59486490b5810239
MiguelQuispe <fisicomiguel@gmail.com>
2021-02-27 12:41:43 -0500

    actualizacion

--- main.ino
+++ main.ino
@@ -14,8 +14,6 @@ String FirmwareVer = {
 #define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
 #define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
 
-//#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
-//#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
 
 void connect_wifi();
 void firmwareUpdate();
